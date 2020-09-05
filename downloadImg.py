cb12ccab2386e32630b8d08d416d2fc74670c48f
Threch422 <69102888+Threch422@users.noreply.github.com>
2020-09-05 19:38:28 +0900

    Update downloadImg.py

--- downloadImg.py
+++ downloadImg.py
@@ -6,12 +6,12 @@ import login as l
 import preset as p
 import user_info as user
 
-def getAuthorImgDict(ID):
+def getAuthorImgDict(author_ID):
     author_Img_Dict_header = {
-    "referer": "https://www.pixiv.net/ajax/user/" + ID + "/profile/all?lang=zh_tw",
+    "referer": "https://www.pixiv.net/ajax/user/" + author_ID + "/profile/all?lang=zh_tw",
     "cookie" : p.cookie
     }
-    request = l.s.get("https://www.pixiv.net/ajax/user/" + ID + "/profile/all?lang=zh_tw", headers = author_Img_Dict_header)
+    request = l.s.get("https://www.pixiv.net/ajax/user/" + author_ID + "/profile/all?lang=zh_tw", headers = author_Img_Dict_header)
     global false, null, true
     false = 'False'
     null = 'None'
@@ -20,8 +20,17 @@ def getAuthorImgDict(ID):
     author_img_dict = list(author_img_dict.get('illusts', {}).keys())
     return author_img_dict
 
-def downloadImg(author_img_dict):
+def downloadImg(author_img_dict, author_ID):
     img_dict = []
+    # Creating new directory
+    author_page = l.s.get('https://www.pixiv.net/users/{}'.format(author_ID), headers = {
+        'referer': 'https://www.pixiv.net/users/{}'.format(author_ID)
+    }).text
+    author_name = re.findall('<title>(.*?) - pixiv</title>',author_page)[0]
+    if not(os.path.isdir(user.path + author_name)):
+        os.mkdir(user.path + author_name)
+    new_path = user.path + '/' + author_name + '/'
+
     for i in range(len(author_img_dict)):
         target = "https://www.pixiv.net/artworks/" + author_img_dict[i]
         target = l.s.get(target).text
@@ -36,7 +45,8 @@ def downloadImg(author_img_dict):
         }
         img_response = l.s.get(img_dict[j], headers = header)
         img = img_response.content
-        with open(user.path + str(j) + '.jpg', 'wb') as jpg:
+        file_name = new_path + '{}'.format(author_img_dict[j]) + '.jpg'
+        with open(file_name, 'wb') as jpg:
             jpg.write(img)
             print("Downloaded: " + str(j+1) + " / " + str(len(img_dict)))
         if (j % 40 == 0):
